69567abceac7981d55dab0b47e623ef3f8d29160
Gumn4m1 <98003893+Pankaj0038@users.noreply.github.com>
2023-10-01 17:54:33 +0530

    Add files via upload

---
+++ b0flow.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+
+void flag(char *li[],int val){
+       	for(int i=0;i<10;i++){
+		printf("%d\n",val);	
+	}
+}
+int main(){
+	char buffer[6];
+	int val = 0;
+	printf("Spells won't allow you to go further !!");
+	gets(buffer);
+	printf("%d\n",val);
+	char *li[] = {"F","l","a","g","{","H","4","c","k","}"};
+	if(val!=0){
+		flag(li,val);
+	}
+	else{
+		printf("\nMay be a soul of deadbeef can help you to get in!");
+	}
+	return 0;
+	
+}
