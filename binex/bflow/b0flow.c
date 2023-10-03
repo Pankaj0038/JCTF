94bdf0cf409e0af3591e0c582003c0490a6ff061
Gumn4m1 <98003893+Pankaj0038@users.noreply.github.com>
2023-10-03 21:21:42 +0530

    Add files via upload

---
+++ binex/bflow/b0flow.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char buffer[100];
+	int val = 0;
+	printf("Spells won't allow you to go further !!");
+	gets(buffer);
+	printf("%d\n",val);
+	char *flag = "JCTF{W45_7h47_50_345Y}";
+	if(val!=0){
+		printf(flag);
+	}
+	else{
+		printf("\nMay be broken bariers can help you to get in!");
+	}
+	return 0;
+	
+}
