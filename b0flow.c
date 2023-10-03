07bf21dce549a59386f4738ee60637a4ec52f80f
Gumn4m1 <98003893+Pankaj0038@users.noreply.github.com>
2023-10-03 21:13:36 +0530

    Update b0flow.c

--- b0flow.c
+++ b0flow.c
@@ -1,24 +1,19 @@
 #include<stdio.h>
 #include<string.h>
 
-void flag(char *li[],int val){
-       	for(int i=0;i<10;i++){
-		printf("%d\n",val);	
-	}
-}
 int main(){
-	char buffer[6];
-	int val = 0;
-	printf("Spells won't allow you to go further !!");
-	gets(buffer);
-	printf("%d\n",val);
-	char *li[] = [redacted];
-	if(val!=0){
-		flag(li,val);
-	}
-	else{
-		printf("\nMay be broken boundaries can help you to get in!");
-	}
-	return 0;
-	
+        char buffer[6];
+        int val = 0;
+        printf("Spells won't allow you to go further !!");
+        gets(buffer);
+        printf("%d\n",val);
+        char *flag = "JCTF{redacted}";
+        if(val!=0){
+                printf(flag);
+        }
+        else{
+                printf("\nMay be broken bariers can help you to get in!");
+        }
+        return 0;
+
 }
